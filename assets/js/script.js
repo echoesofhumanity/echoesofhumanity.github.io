6417b3eb375c5ed8a15038820ac4f567acbae621
echoesofhumanity <echoesofhumanity.global@gmail.com>
2026-08-07 15:50:02 +0300

    Enable shrinking header on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,5 +18,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const savedLanguage = localStorage.getItem("language") || "en";
     loadLanguage(savedLanguage);
     });
+// ==========================
+// Shrink Header on Scroll
+// ==========================
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 40) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
 
     
